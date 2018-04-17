27b6a1cc6b12065648ebe857c90e3f1f9748322d
NeverMore93 <1305882275@qq.com>
2018-04-17 17:59:49 +0800

    Initial commit

---
+++ src/main/java/com/ywt/projecta/ProjectaApplication.java
@@ -0,0 +1,12 @@
+package com.ywt.projecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectaApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ywt/projecta/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.ywt.projecta.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    String home() {
+       return  "Hello World!";
+    }
+}
